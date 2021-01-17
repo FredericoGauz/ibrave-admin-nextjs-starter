819000a6cb9e8a4182c81dac98f0494251886c82
Fred <frgauz@gmail.com>
2021-01-17 08:40:43 -0800

    prettier tab ajustment

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,2 @@
-export { default as NavBar } from './NavBar'
-export * from './seo'
+export { default as NavBar } from './NavBar';
+export * from './seo';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
-const colors = require('tailwindcss/colors')
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors');
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  },
-  darkMode: false,
-  theme: {
-    fontFamily: {
-      sans: ['Inter', ...fontFamily.sans],
+    purge: {
+        content: ['./src/**/*.{js,ts,jsx,tsx}'],
     },
-    extend: {
-      colors: {
-        violet: colors.violet,
-      },
+    darkMode: false,
+    theme: {
+        fontFamily: {
+            sans: ['Inter', ...fontFamily.sans],
+        },
+        extend: {
+            colors: {
+                violet: colors.violet,
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
+    variants: {},
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+};
 
 /**
  * === Already Included Colors ===
