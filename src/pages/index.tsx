7c8fb4cc06123af0275798bd5275ed91085c5537
Fred <frgauz@gmail.com>
2021-01-17 12:03:33 -0800

    remove outer layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,28 @@
 import { useSession } from 'next-auth/client';
-import Image from 'next/image';
 import { ReactNode } from 'react';
 // import { CTA } from '../components/CTA';
 export default function Home() {
     const [session] = useSession();
     return (
-        <div className="divide-y divide-gray-100">
+        <main>
             {/* <CTA /> */}
-            <main>
-                <div className="p-10">
-                    {session && (
-                        <p>
-                            Hello,{' '}
-                            <b>{session.user.email ?? session.user.name}</b>
-                        </p>
-                    )}
-                </div>
-            </main>
-        </div>
+            <div className="p-10">
+                {session && (
+                    <p>
+                        Hello, <b>{session.user.email ?? session.user.name}</b>
+                    </p>
+                )}
+            </div>
+        </main>
     );
 }
 
 function HomeLayout({ children }: { children: ReactNode }) {
     return (
         <>
-            <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                {children}
-            </div>
+            {/* <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8"> */}
+            {children}
+            {/* </div> */}
         </>
     );
 }
